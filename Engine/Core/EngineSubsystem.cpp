1b608c86f8c795516fc7ddc792dd7d6475974f26
İsmail Özsaygı <devozsaygi@gmail.com>
2025-04-21 23:56:50 +0300

    Added base engine subsystem class

---
+++ Engine/Core/EngineSubsystem.cpp
@@ -0,0 +1,13 @@
+#include "EngineSubsystem.h"
+
+namespace Engine
+{
+    EngineSubsystemInitializationStatus EngineSubsystem::Initialize()
+    {
+        return SuccessfullyInitialized;
+    }
+
+    void EngineSubsystem::Shutdown()
+    {
+    }
+} // namespace Engine
