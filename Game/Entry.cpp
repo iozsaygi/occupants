e9a5028bbf07f5b4d09fe0eb96a9b1d46772d62f
İsmail Özsaygı <devozsaygi@gmail.com>
2025-04-25 16:06:43 +0300

    Added engine initialization calls to Game client

--- Game/Entry.cpp
+++ Game/Entry.cpp
@@ -1,3 +1,11 @@
+#include "Engine.h"
+
 int main( int argc, char* argv[] )
 {
+    // Create and try to initialize engine entry and related subsystems.
+    Engine::EngineEntry engineEntry;
+    engineEntry.TryInitializingSubsystems();
+    engineEntry.ShutdownSubsystems();
+
+    return 0;
 }
