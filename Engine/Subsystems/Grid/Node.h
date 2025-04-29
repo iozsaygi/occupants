3ecd02c3504f4ffb75e69255d63705735c517c31
İsmail Özsaygı <devozsaygi@gmail.com>
2025-04-29 09:41:26 +0300

    Added basic node struct

---
+++ Engine/Subsystems/Grid/Node.h
@@ -0,0 +1,22 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include "Mathematics/Vector2D.h"
+
+#define INVALID_NODE_ID -1
+#define NODE_NEIGHBOR_CAPACITY 4
+
+namespace Engine
+{
+    struct Node
+    {
+        // Unique ID representing the node within the grid. Can be useful if node equality checks required.
+        int ID;
+
+        Vector2D Position;
+
+        int Neighbors[NODE_NEIGHBOR_CAPACITY];
+    };
+} // namespace Engine
+
+#endif // NODE_H
