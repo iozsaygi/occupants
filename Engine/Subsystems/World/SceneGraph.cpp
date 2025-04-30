66d301a6d36e1e52fc32429ca57789fe307ccd8c
İsmail Özsaygı <devozsaygi@gmail.com>
2025-04-30 10:21:26 +0300

    Added basic methods for scene graph

--- Engine/Subsystems/World/SceneGraph.cpp
+++ Engine/Subsystems/World/SceneGraph.cpp
@@ -1,6 +1,46 @@
 #include "SceneGraph.h"
+#include <cassert>
 
 namespace Engine
 {
+    SceneGraph::SceneGraph( const int actorCapacity )
+    {
+        assert( actorCapacity > 0 );
 
-}
+        m_Actors = new Actor*[ actorCapacity ];
+        Length = 0;
+    }
+
+    SceneGraph::~SceneGraph()
+    {
+        delete[] m_Actors;
+    }
+
+    bool SceneGraph::TryRegisterActor( Actor* actor ) const
+    {
+        assert( actor != nullptr );
+
+        int availableSceneGraphIndex = -1;
+        if ( !TryFindFirstAvailableSceneGraphIndex( availableSceneGraphIndex ) ) return false;
+
+        m_Actors[ availableSceneGraphIndex ] = actor;
+
+        return true;
+    }
+
+    bool SceneGraph::TryFindFirstAvailableSceneGraphIndex( int& index ) const
+    {
+        for ( int i = 0; i < Length; i++ )
+        {
+            if ( m_Actors[ i ] == nullptr )
+            {
+                index = i;
+                return true;
+            }
+        }
+
+        // This is indicating that we reached our actor capacity.
+        return false;
+    }
+
+} // namespace Engine
--- Engine/Subsystems/World/SceneGraph.h
+++ Engine/Subsystems/World/SceneGraph.h
@@ -1,9 +1,26 @@
 #ifndef SCENE_GRAPH_H
 #define SCENE_GRAPH_H
 
+#include "Actor.h"
+
 namespace Engine
 {
+    // The manager of the actors within the scene. No matter if they are visible (rendered) or not.
+    class SceneGraph final
+    {
+    public:
+        int Length;
 
-}
+        explicit SceneGraph( int actorCapacity );
+        ~SceneGraph();
+
+        bool TryRegisterActor( Actor* actor ) const;
+
+    private:
+        Actor** m_Actors = nullptr;
+
+        bool TryFindFirstAvailableSceneGraphIndex( int& index ) const;
+    };
+} // namespace Engine
 
 #endif // SCENE_GRAPH_H
