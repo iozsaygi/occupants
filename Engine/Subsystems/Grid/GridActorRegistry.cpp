e6c5c79b70760721e74cb50a1dd064661b4a2533
İsmail Özsaygı <devozsaygi@gmail.com>
2025-05-05 08:49:55 +0300

    Added initial APIs for grid actor registry

--- Engine/Subsystems/Grid/GridActorRegistry.cpp
+++ Engine/Subsystems/Grid/GridActorRegistry.cpp
@@ -1,6 +1,40 @@
 #include "GridActorRegistry.h"
+#include <assert.h>
 
 namespace Engine
 {
+    GridActorRegistry::GridActorRegistry()
+    {
+        m_Registry = std::map<Node, Actor*>();
+    }
 
-}
+    bool GridActorRegistry::TryRegisterActorToNode( Node& node, Actor* actor )
+    {
+        assert( actor != nullptr );
+
+        const auto lookupQuery = m_Registry.find( node );
+        if ( lookupQuery == m_Registry.end() )
+        {
+            // Failed to find actor on the target node, it is safe to register.
+            m_Registry.insert( { node, actor } );
+            return true;
+        }
+
+        // The target node already contains another actor.
+        return false;
+    }
+
+    bool GridActorRegistry::TryGetNodeOccupiedByActor( const Actor* actor, Node& node ) const
+    {
+        for ( const auto pair: m_Registry )
+        {
+            if ( pair.second == actor )
+            {
+                node = pair.first;
+                return true;
+            }
+        }
+
+        return false;
+    }
+} // namespace Engine
--- Engine/Subsystems/Grid/GridActorRegistry.h
+++ Engine/Subsystems/Grid/GridActorRegistry.h
@@ -1,10 +1,22 @@
 #ifndef GRID_ACTOR_REGISTRY_H
 #define GRID_ACTOR_REGISTRY_H
 
+#include <map>
+#include "Node.h"
+#include "Subsystems/World/Actor.h"
+
 namespace Engine
 {
     class GridActorRegistry final
     {
+    public:
+        GridActorRegistry();
+
+        bool TryRegisterActorToNode( Node& node, Actor* actor );
+        bool TryGetNodeOccupiedByActor( const Actor* actor, Node& node ) const;
+
+    private:
+        std::map<Node, Actor*> m_Registry;
     };
 } // namespace Engine
 
