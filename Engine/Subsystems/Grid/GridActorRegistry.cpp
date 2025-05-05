6a3930ff42e48147a98624533b73558eae43a8b2
İsmail Özsaygı <devozsaygi@gmail.com>
2025-05-05 08:21:22 +0300

    Added basics of grid actor registry

---
+++ Engine/Subsystems/Grid/GridActorRegistry.cpp
@@ -0,0 +1,6 @@
+#include "GridActorRegistry.h"
+
+namespace Engine
+{
+
+}
---
+++ Engine/Subsystems/Grid/GridActorRegistry.h
@@ -0,0 +1,11 @@
+#ifndef GRID_ACTOR_REGISTRY_H
+#define GRID_ACTOR_REGISTRY_H
+
+namespace Engine
+{
+    class GridActorRegistry final
+    {
+    };
+} // namespace Engine
+
+#endif // GRID_ACTOR_REGISTRY_H
